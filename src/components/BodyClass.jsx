c606b77e633f2de2885b0bbc83d10986c3e3b282
baselAhmed2 <baselahmed363@gmail.com>
2026-03-23 06:48:02 +0200

    وسطنه

--- src/components/BodyClass.jsx
+++ src/components/BodyClass.jsx
@@ -6,7 +6,13 @@ import { usePathname } from 'next/navigation';
 export default function BodyClass() {
   const pathname = usePathname();
   useEffect(() => {
-    const isLogin = pathname === '/' || (pathname && pathname.startsWith('/forgettenpassword'));
+    const isLogin = 
+      pathname === '/' || 
+      (pathname && (
+        pathname.startsWith('/forgettenpassword') || 
+        pathname.startsWith('/register') || 
+        pathname.startsWith('/verify-otp')
+      ));
     if (isLogin) {
       document.body.classList.add('login-body');
       document.body.classList.remove('dashboard-body');
